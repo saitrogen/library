3c2627ba33d1fa7c9a9e4aeebd9b6aa74ac1c599
saitrogen <ashiquvava381@gmail.com>
2024-09-09 01:24:36 +0530

    ,

--- src/app/(pages)/resources/page.tsx
+++ src/app/(pages)/resources/page.tsx
@@ -1,8 +1,13 @@
 
 export default function page() {
   return (
+    // a html page according to sidebar resources
+
     <>
-    
+    <div className="mx-auto p-4">
+      <h2>digital library</h2>
+      <p></p>
+    </div>
     </>
   );
 }
--- src/components/sections/sidebar.tsx
+++ src/components/sections/sidebar.tsx
@@ -7,21 +7,22 @@ import data from "../../data/sidebar/resources.json"
 
 const Sidebar = () => {
   return (
-    <><nav className="overflow-auto">
-      
-      {data.map((item) => (
-        <>
-        <h5 className=" mt-8 mb-8 lg:mb-3 font-semibold text-md text-slate-900 dark:text-emerald-200">{item.title}</h5>
-        <ul className="space-y-6 lg:space-y-2 border-l border-slate-100 dark:border-emerald-700 text-sm">
-          {item.children.map((child) => (
-            <li><Link className="block border-l pl-4 -ml-px border-transparent hover:border-slate-400 dark:hover:border-emerald-500 text-slate-700 hover:text-slate-900 dark:text-emerald-400 dark:hover:text-emerald-300" href={""}>{child.sub}</Link></li>
-          ))}
-        </ul>
-        </>
-      ))}
+    <>
+      <nav className="overflow-auto">
 
-    </nav>
-      
+        {data.map((item) => (
+          <>
+            <h5 className=" mt-8 mb-8 lg:mb-3 font-semibold text-md text-slate-900 dark:text-emerald-200">{item.title}</h5>
+            <ul className="space-y-6 lg:space-y-2 border-l border-slate-100 dark:border-emerald-700 text-sm">
+              {item.children.map((child, index) => (
+                <li key={index}><Link className="block border-l pl-4 -ml-px border-transparent hover:border-slate-400 dark:hover:border-emerald-500 text-slate-700 hover:text-slate-900 dark:text-emerald-400 dark:hover:text-emerald-300" key={child.sub} href={""}>{child.sub}</Link></li>
+              ))}
+            </ul>
+          </>
+        ))}
+
+      </nav>
+
     </>
   );
 };
